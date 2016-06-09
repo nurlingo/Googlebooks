c1ccc46143f8c7adf5a635c666e0aefdb74390a7
Nursultan Askarbekuly <Noor@nursultans-macbook-air-2.local>
2016-06-09 11:41:49 +0600

    Initial Commit

---
+++ Google Book AppTests/Google_Book_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Google_Book_AppTests.swift
+//  Google Book AppTests
+//
+//  Created by Nursultan Askarbekuly on 09/06/2016.
+//  Copyright © 2016 Nursultan Askarbekuly. All rights reserved.
+//
+
+import XCTest
+@testable import Google_Book_App
+
+class Google_Book_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
